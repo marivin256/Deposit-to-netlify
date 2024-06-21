f79a047349d70dceb00b9e057172272f8354712f
marvin256 <marvintomusange@gmail.com>
2024-06-21 18:19:24 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 function App (){
     return(
-        <h1 className="text-red-500 text-center">HELLO WORLD</h1>
+        <>
+        <h1 className="text-red-500 text-center font-bold">HELLO WORLD</h1>
+        <details className="text-center text-blue-500">
+            <summary>Read</summary>
+            <p>My name is Tom</p>
+        </details>
+        </>
     )
 }
 export default App;
