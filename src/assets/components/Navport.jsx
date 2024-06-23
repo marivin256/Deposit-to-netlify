e33b3fa37e2c1d482e6275ccabc1e4a8842964c0
marvin256 <marvintomusange@gmail.com>
2024-06-23 11:55:56 +0300

    6th commit

--- src/assets/components/Navport.jsx
+++ src/assets/components/Navport.jsx
@@ -7,14 +7,14 @@ function Port (){
     return(
 
 <div className="bg-black sticky top-0 z-50">
- <nav className=" border-b border-yellow-500">
+ <nav className=" border-b border-yellow-500 ">
        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-">
          <div className="flex h-20 items-center justify-between ">
            <div
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
                <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img
-                className="h-10 w-auto"
+                className="h-10 w-auto z-10 ml-2"
                 src={logo}
                 alt="Marvitech logo"
               />
@@ -45,11 +45,7 @@ function Port (){
                   >Skills</NavLink>
                 
                 
-                <NavLink
-                  to="/contact"
-                  className={linkClass}
-                  >Contact</NavLink>
-                
+               
               </div>
             </div>
 
